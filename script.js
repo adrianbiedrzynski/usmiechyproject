e4335982d0721850d09aa5289176b53fc3d79859
Adrian Biedrzynski <biedrzynski.adrian@gmail.com>
2019-07-18 23:15:05 +0200

    feat.correct js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('.site_header-navbar');
 
-button = addEventListener('click', () => {
+button.addEventListener('click', () => {
     const navigation = document.querySelector('.site_header-navigation');
     navigation.classList.toggle('site_header-navigation-active');
     const bar = document.querySelector('.site_header-navbar');
