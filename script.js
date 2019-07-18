133ccdaafb4e7fb41e89df0a68d2ae523ea5ef49
Adrian Biedrzynski <biedrzynski.adrian@gmail.com>
2019-07-18 20:53:52 +0200

    feat.menu_mobile

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.site_header-navbar');
+
+button = addEventListener('click', () => {
+    const navigation = document.querySelector('.site_header-navigation');
+    navigation.classList.toggle('site_header-navigation-active');
+    const bar = document.querySelector('.site_header-navbar');
+    bar.classList.toggle('site_header-navbar-active')
+})
